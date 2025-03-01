8d2ae68b8544e35092211ad6614be44fda3797b8
Dwood04 <dwood112204@gmail.com>
2025-03-01 13:41:22 -0500

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -1,1 +1,33 @@
+class Todo {
+
+  constructor(Title, Date) {
+    this.Title = Title;
+    this.Date = Date;
+    this.Desc = '';
+  }
+
+  editTitle(Title) {
+    this.Title = Title;
+  }
+
+  editDate(Date) {
+    this.Date = Date;
+  }
+
+  editDesc(Desc) {
+    this.Desc = Desc;
+  }
+
+  getTitle() {
+    return this.Title;
+  }
+  
+  getDate() {
+    return this.Date;
+  }
+
+  getDesc() {
+    return this.Desc;
+  }
+}
 
