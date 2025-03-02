e191c15ec5558cb1da467a1c120b2421dc182fe6
Dwood04 <dwood112204@gmail.com>
2025-03-01 20:14:19 -0500

    Big fixes, remove bulk, added test buttons

--- calendar.js
+++ calendar.js
@@ -1,69 +1,23 @@
-var TaskList = new list();
+var TaskList = [];
 
-class list {
-  constructor(item){
-    var list = [];
-    this.list.push(item);
-  }
-  addItem(item){
-    this.list.push(item);
-  }
-  removeItem(item){
-    this.list.pop(item);
-  }
-  getList(){
-    return this.list;
-  }
-  clearList(){
-    this.list = [];
-  }
-  getItem(index){
-    return this.list[index];
-  }
-  editItem(index, item){
-    this.list[index] = item;
-  }
-  getLength(){
-    return this.list.length;
-  }
-}
-class Todo {
-
-  constructor(Title, Date) {
-    this.Title = Title;
-    this.Date = Date;
-    this.Desc = '';
-  }
-
-  editTitle(Title) {
-    this.Title = Title;
-  }
-
-  editDate(Date) {
-    this.Date = Date;
-  }
-
-  editDesc(Desc) {
-    this.Desc = Desc;
-  }
-
-  getTitle() {
-    return this.Title;
-  }
-  
-  getDate() {
-    return this.Date;
-  }
-
-  getDesc() {
-    return this.Desc;
-  }
-}
 function addTodo() {
   var title = document.getElementById("todo").value;
   var date = document.getElementById("date").value;
-  var todo = new Todo (title, date);
-  list.addItem(Todo);
-  
+  var desc = document.getElementById("desc").value;
+  var todo = new Todo (title, date, desc);
+  TaskList.push(todo);
+  return false;
 }
 
+function myFunction(){
+  if(TaskList.length > 0){
+    document.body.style.backgroundColor = "lightblue";
+  }
+
+}
+function myFunction1(){
+  if(TaskList.pop != null){
+    document.body.style.backgroundColor = "lightgreen";
+  }
+
+}
